ce2a6bd8f7913e7ce65336beab3cb529ddf94bda
quantneuro <aytshch@gmail.com>
2026-09-24 20:48:46 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.go
@@ -0,0 +1,29 @@
+func climbStairs(n int) int {
+
+	cache:=make(map[int]int)
+	
+	var recur func(n int)int
+	
+	recur=func(n int)int{
+	if n<0{
+		return 0
+	}
+    if n==0{
+		return 1
+	}
+	if val,ok:=cache[n];ok{
+		return val
+	}
+
+	result:=climbStairs(n-1)+climbStairs(n-2)
+
+	cache[n]=result
+
+	return result
+
+	}
+
+	return recur(n)
+	
+
+}
