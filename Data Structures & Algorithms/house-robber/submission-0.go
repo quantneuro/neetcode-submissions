289e9570366be5d7b0b0c618a6ea616faaf6ab89
quantneuro <aytshch@gmail.com>
2026-09-25 10:23:43 -0400

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.go
@@ -0,0 +1,25 @@
+func rob(nums []int) int {
+    
+	cache:=make(map[int]int)
+	top =len(nums)
+	var recur func(n int)int
+
+	recur=func(n int)int{
+		if n>=top{
+			return 0
+		}
+
+		if val,ok:=cache[n];ok{
+			return val
+		}
+
+		robn:=nums[n]+recur(n+2)
+		skipn:=recur(n+1)
+		result:=max(skipn,robn)
+		cache[n]=result
+		return result
+
+	}
+	return recur(0) 
+
+}
